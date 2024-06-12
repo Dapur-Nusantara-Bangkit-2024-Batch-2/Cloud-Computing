3009a153ba3b32ae8a2e77eddae735f5c95de1d3
Zuhud Ulin Nuha <ulinuha@student.ub.ac.id>
2024-06-12 14:11:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const foods = require("./foods.json");
 const app = express();
-const port = 3000; // You can choose any port you prefer
-
-// Define a route handler for the default home page
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const port = 3000;
 
 // Start the server
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`);
+app.listen(port, () => console.log(`App is running on http://localhost:${port}`));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/foods", (req, res) => {
+  res.json(foods);
+});
+
+// Routes
+app.get("/foods", (req, res) => {
+  res.json(foods);
+});
+
+app.get("/foods/:id", (req, res) => {
+  const foodId = parseInt(req.params.id, 10);
+  const food = foods.find((food) => food.id === foodId);
+
+  if (food) {
+    res.json(food);
+  } else {
+    res.status(404).json({ message: "Food not found" });
+  }
 });
